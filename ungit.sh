b5dc239147d9de0c484e6e213f303f9d2d7c5577
Emmanuel Frécon <efrecon@gmail.com>
2023-11-17 11:16:11 +0100

    Remove unecessary init code

--- ungit.sh
+++ ungit.sh
@@ -4,34 +4,6 @@
 
 set -eu
 
-# Find out where dependent modules are and load them at once before doing
-# anything. This is to be able to use their services as soon as possible.
-
-# This is a readlink -f implementation so this script can (perhaps) run on MacOS
-abspath() {
-  is_abspath() {
-    case "$1" in
-      /* | ~*) true;;
-      *) false;;
-    esac
-  }
-
-  if [ -d "$1" ]; then
-    ( cd -P -- "$1" && pwd -P )
-  elif [ -L "$1" ]; then
-    if is_abspath "$(readlink "$1")"; then
-      abspath "$(readlink "$1")"
-    else
-      abspath "$(dirname "$1")/$(readlink "$1")"
-    fi
-  else
-    printf %s\\n "$(abspath "$(dirname "$1")")/$(basename "$1")"
-  fi
-}
-
-UNGIT_ROOTDIR=$( cd -P -- "$(dirname -- "$(command -v -- "$(abspath "$0")")")" && pwd -P )
-
-
 # Level of verbosity, the higher the more verbose. All messages are sent to the
 # stderr.
 UNGIT_VERBOSE=${UNGIT_VERBOSE:-0}
